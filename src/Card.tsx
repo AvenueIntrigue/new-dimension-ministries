333023274ccda81c78d85432d30e9cb9808b2b88
“Noah <noah@brightlightscreative.com>
2024-07-11 15:46:53 -0400

    Refactor Card Optimize Vertical Video Add Card Props

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,8 +1,21 @@
+import React from "react";
 import Button from "./Button";
+import './Card.css'
 
 
 
-function Card() {
+type CardProps = {
+
+
+  iframeUrl: string;
+  iframeTitle: string;
+  cardTitle: string;
+  linkHref: string;
+
+}
+
+
+const Card: React.FC<CardProps> = ({iframeUrl,iframeTitle, cardTitle, linkHref}) => {
 
 
 
@@ -12,17 +25,20 @@ function Card() {
 
     <div className="card justify-center">
 
-      <div className="ratio ratio-16x9">
-        <iframe style={{ borderRadius: "1%" }} width="100%" height="auto" src="https://www.youtube.com/embed/lo0jNM7ov4c?si=GWITWsYGYfR1jcbV" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      </div>
+      
 
       <div className="card-body">
-        <h2 className="card-title">LATEST YOUTUBE VIDEOS</h2>
+      <div className="ratio">
+        <iframe  width="100%" height="100%" src={iframeUrl} title={iframeTitle} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      </div>
+        <h2 className="card-title">{cardTitle}</h2>
         <p className="card-text">Tune-In Daily for the latest sermons, shorts, & more! </p>
-        <a href="https://www.youtube.com/@ErnestWashington-sk8mq" target="blank">
+        <a href={linkHref} target="blank">
           <Button name="WATCH MORE HERE!"></Button>
         </a>
       </div>
+
+
 
 
 
